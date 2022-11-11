e0803fad4df9eb318aa113c1ddb72cb2b927352b
Roei Shemesh <83763662+roeishemesh@users.noreply.github.com>
2022-11-11 16:36:52 +0200

    Update final_bot.py

--- final_bot.py
+++ final_bot.py
@@ -9,19 +9,17 @@ bot = telegram.Bot(token="1598974200:AAGZdylImna2HeEiJ7929OQqo3uC3dJ0qF8")
 def start(update, context):
     bot_welcome = """
     Hii!
-    welcome to stock telegram bot!
-    you can give index name, stock name or stock symbol.
-    i will give you the stock price
-    and if it go up or down today.
-    if you lock for crypto coin:
-    please write "crypto" after
-    your crypto name (BTC crypto).
-    all the information is from
-    investing.com """
+    Welcome to stock exchange telegram bot!
+    You can write an index, a stock, a stock symbol or a cryptocurrency name,
+    and you will receive her value and her daily change.
+    If you are looking for cryptocurrency - 
+    write "crypto" after the cryptocurrency name or symbol
+    For example - "BTC crypto".
+    All the information is belonging to the website www.investing.com"""
     context.bot.send_message(chat_id=update.message.chat_id, text=bot_welcome)
 
 
-def getinfo(update, context):
+def getinfo(update):
     update.message.reply_text(choose.stock_or_crypto(update.message.text))
 
 
